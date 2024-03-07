884f22a40ad6aaa3f98b73c261ed4ee6b37a0de3
JuanManulGomezOchoa <utm23090606@utma.edu.mx>
2024-03-07 14:56:53 -0600

    first commit

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+import {Router} from "express";
+const router = Router();
+
+let users= [
+    {
+        id:"1",
+        name:"jesus",
+        address:"aña",
+        age:85,
+    }
+]
+
+//POST
+router.post('/users/create', (req,res)=>{ 
+    //BODY
+    users.push(req.body)
+    res.send("ok");
+})
+
+
+router.get('/users', (req,res)=>{
+    res.status(200).json(users)
+});
+
+//PUT
+router.put('/users/update/:id', (req,res)=>{
+    const user = users.find(u=>u.id === req.params.id);
+    if(!user){
+        res.status(404).send("El usuario no se encuentra!!!");
+        return;
+    }
+
+    const updateUser = {...user, ...req.body}
+
+    users = users.map(u=>u.id === req.params.id ? updateUser : u);
+    res.status(200).send("Usuario actualizado con exito!!!");
+})
+
+export default router;
